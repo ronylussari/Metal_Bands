ada5d4559deaed8d217c9394af42e172c048f443
Rony Lussari <rony.lussari@outlook.com>
2017-05-07 18:12:04 -0300

    init

---
+++ data_cleaning.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+
+def read_csv(filename):
+    reader = pd.read_csv(filename, encoding = "latin1")
+    return reader
+
+
+
+
+
+
+
+
+
+
---
+++ main.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import numpy as np
+
+import data_cleaning as dc
+
+bands = dc.read_csv('metal_bands_2017.csv')
